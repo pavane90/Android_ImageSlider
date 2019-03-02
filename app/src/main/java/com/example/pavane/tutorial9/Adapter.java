7b2efd89579c4943c08c17d4d332e67d60ecf463
Lee, Byungman <pavane90@gmail.com>
2019-03-02 23:49:35 +0900

    Initial commit

---
+++ app/src/main/java/com/example/pavane/tutorial9/Adapter.java
@@ -0,0 +1,50 @@
+package com.example.pavane.tutorial9;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class Adapter extends PagerAdapter {
+
+    private int[] images = {R.drawable.four, R.drawable.four};
+    private LayoutInflater inflater;
+    private Context context;
+
+    public Adapter(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return images.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object o) {
+        return view == ((LinearLayout) o);
+    }
+
+    @NonNull
+    @Override
+    public java.lang.Object instantiateItem(@NonNull ViewGroup container, int position) {
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View v = inflater.inflate(R.layout.slider, container, false);
+        ImageView imageView = (ImageView) v.findViewById(R.id.imageView);
+        TextView textView = (TextView) v.findViewById(R.id.textView);
+        imageView.setImageResource(images[position]);
+        textView.setText((position+1) + "번째 이미지 입니다.");
+        container.addView(v);
+        return v;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull java.lang.Object object) {
+        container.invalidate();
+    }
+}
